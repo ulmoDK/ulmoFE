1fee89e1841e783ea432cd83f54e0e939f19cbb7
ulmoDK <ulrichamortensen@gmail.com>
2019-12-09 22:36:01 +0100

    Added very simple solver class

--- Solver.py
+++ Solver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 23 23:16:28 2019
+Created on Mon Dec  9 22:34:37 2019
 
 @author: ulmo
 """
@@ -11,45 +11,7 @@ from scipy.linalg import eig
 
 from Elements import BeamElem
 from Mesh import BeamMesh
-
-class Assembly:
-    
-    def __init__(self,mesh):
-
-        self.mesh = mesh
-        self.K = self.createGlobal("K")
-        self.M = self.createGlobal("M")
-        #print(self.K)
-        
-    def createGlobal(self,GM="K"):
-        G = np.zeros((self.mesh["numNodes"]*2,self.mesh["numNodes"]*2))
-        
-        
-        localDofs = list(range(4))
-        for elem in self.mesh["mesh"]:
-            globalDofs = elem["dofs"]
-            for globalRow,localRow in zip(globalDofs,localDofs):
-                for globalCol,localCol in zip(globalDofs,localDofs):
-                    G[globalRow,globalCol] += elem[GM][localRow,localCol]
-                    
-        return G
-        
-
-
-    def imposeBC(self,bcNodes):
-
-        
-        for node in bcNodes:
-            self.K[node,:]=0.0
-            self.K[:,node]=0.0
-            self.K[node,node]=1.0
-            
-            self.M[node,:]=0.0
-            self.M[:,node]=0.0
-            
-        
-        
-        return self
+from Assembler import Assembly
 
 
 
